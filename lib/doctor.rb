f4dab4fe743e9741f138f10b9e55fdbc7859cd0c
kelly r maass <kmaass77627@gmail.com>
2020-01-03 22:12:54 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Doctor
 
   attr_reader :name, :appointment, :patient
@@ -18,6 +20,7 @@ class Doctor
   end
 
   def appointments
+    binding.pry 
     Appointment.all.select do |appointment|
       appointment.doctor == self
     end
