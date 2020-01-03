54f81fed708af7f04de21693280393936e60e71c
kelly r maass <kmaass77627@gmail.com>
2020-01-03 21:17:34 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,35 @@
+class Artist  
+  
+  @@all = []
+  
+  attr_accessor :name, :artist, :genre 
+  
+  def initialize(name)
+    @name = name
+    @@all << self 
+    @songs = []
+  end 
+  
+  def self.all
+    @@all
+  end 
+  
+ def new_song(name, genre)
+    song = Song.new(name, self, genre)
+    self.songs.last
+  end
+  
+   def songs
+    Song.all.select do |song|
+      song.artist == self
+    end
+  end
+    
+  def genres
+    Song.all.map do |song|
+      song.genre
+    end
+  end
+
+end 
+    
---
+++ lib/genre.rb
@@ -0,0 +1,28 @@
+class Genre 
+  
+  attr_accessor :name, :genre, :artist 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self 
+  end 
+  
+  def self.all
+    @@all
+  end 
+  
+  def songs 
+    Song.all.select do |song|
+      song.genre == self  
+  end 
+  
+  def artists
+    Song.all.map do |song|
+      song.artist
+    end 
+  end 
+  
+  end 
+end 
